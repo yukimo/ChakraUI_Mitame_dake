1f40067d4a4d57d2eb1303452ea6f68dbb7587c5
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-12-02 22:32:36 +0900

    login login

--- src/compornents/pages/Login.tsx
+++ src/compornents/pages/Login.tsx
@@ -1,4 +1,19 @@
 import { memo, VFC } from "react";
+import {Flex, Box, Heading, Divider, Stack, Input} from "@chakra-ui/react";
+
+import{PrimaryButton} from "../atoms/button/PrimaryButton"
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+     <Flex align="center" justify="center" height="100vh">
+    <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+    <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
+    <Divider my={4} />
+ <Stack spacing={6} py={4} px={10}> 
+    <Input placeholder="ユーザーID" />
+       <PrimaryButton>ログイン</PrimaryButton>
+</Stack>
+    </Box>
+  </Flex> 
+ 
+  );
 });
