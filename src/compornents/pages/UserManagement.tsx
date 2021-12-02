e1f19f0df11aeb13a02f4187c684740f2f12006b
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-12-02 23:58:13 +0900

    card_totyu

--- src/compornents/pages/UserManagement.tsx
+++ src/compornents/pages/UserManagement.tsx
@@ -1,16 +1,27 @@
 import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="1px">
-      {[...Array(3)].map(() => (
-        <WrapItem>
-          <div
-            style={{ width: "100px", height: "100px", backgroundColor: "red" }}
-          />
-        </WrapItem>
-      ))}
+    <Wrap>
+      <WrapItem>
+        <Box w="268px" h="260px" bg="white" borderRadius="10px" shadow="md">
+          {" "}
+          <Stack textAlign="center">
+            <Image
+              boxSize="160px"
+              borderRadius="full"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text>じゃけえ</Text>
+            <Text fontSize="lg" fontWeight="bold">
+              Momiyama Yuki
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   );
 });
