9349e28461551a3b7d7d3c80c109b63d353e05e4
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-12-02 22:54:49 +0900

    wraptest wraptest

--- src/compornents/pages/Login.tsx
+++ src/compornents/pages/Login.tsx
@@ -1,19 +1,20 @@
 import { memo, VFC } from "react";
-import {Flex, Box, Heading, Divider, Stack, Input} from "@chakra-ui/react";
+import { Flex, Box, Heading, Divider, Stack, Input } from "@chakra-ui/react";
 
-import{PrimaryButton} from "../atoms/button/PrimaryButton"
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 export const Login: VFC = memo(() => {
   return (
-     <Flex align="center" justify="center" height="100vh">
-    <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
-    <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
-    <Divider my={4} />
- <Stack spacing={6} py={4} px={10}> 
-    <Input placeholder="ユーザーID" />
-       <PrimaryButton>ログイン</PrimaryButton>
-</Stack>
-    </Box>
-  </Flex> 
- 
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
   );
 });
--- src/compornents/pages/UserManagement.tsx
+++ src/compornents/pages/UserManagement.tsx
@@ -1,4 +1,12 @@
 import { memo, VFC } from "react";
+import{Wrap, WrapItem}from"@chakra-ui/react";
+
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return(
+    <Wrap>
+    <WrapItem>
+    <div style={{width:'100px', height:'100px', backgroundColor:'red'}} />
+    </WrapItem>
+    </Wrap>
+  );
 });
