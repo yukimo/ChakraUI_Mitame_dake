684b6cd3ca039c47c37dd3f3c6687d6a093f5499
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-12-02 23:35:39 +0900

    Arrey_example Arreyexample

--- src/compornents/atoms/button/PrimaryButton.tsx
+++ src/compornents/atoms/button/PrimaryButton.tsx
@@ -1,13 +1,23 @@
-import { memo, VFC, ReactNode } from "react";
-import {Button} from "@chakra-ui/react";
-
-type Props={
-children:ReactNode;
+import { memo, VFC, useCallback, ReactNode } from "react";
+import { Button } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+type Props = {
+  children: ReactNode;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-const{children}=props;
+  const history = useHistory();
+  const onClickHome = useCallback(() => history.push("/home"), []);
+
+  const { children } = props;
   return (
-<Button bg="teal.400" color="white" _hover={{opacity:0.8}}>{children}</Button>
+    <Button
+      onClick={onClickHome}
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+    >
+      {children}
+    </Button>
   );
 });
--- src/compornents/pages/UserManagement.tsx
+++ src/compornents/pages/UserManagement.tsx
@@ -1,12 +1,16 @@
 import { memo, VFC } from "react";
-import{Wrap, WrapItem}from"@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
-  return(
-    <Wrap>
-    <WrapItem>
-    <div style={{width:'100px', height:'100px', backgroundColor:'red'}} />
-    </WrapItem>
+  return (
+    <Wrap spacing="1px">
+      {[...Array(3)].map(() => (
+        <WrapItem>
+          <div
+            style={{ width: "100px", height: "100px", backgroundColor: "red" }}
+          />
+        </WrapItem>
+      ))}
     </Wrap>
   );
 });
